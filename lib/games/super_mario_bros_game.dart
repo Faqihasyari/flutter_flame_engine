85f61e00922b99b6448cad6af33698ab80730d0a
Faqihasyari <faqih8158@gmail.com>
2025-08-02 21:58:46 +0700

    push

--- lib/games/super_mario_bros_game.dart
+++ lib/games/super_mario_bros_game.dart
@@ -1,8 +1,11 @@
+import 'package:flame/camera.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:supermariobros/constants/globals.dart';
 
 class SuperMario extends FlameGame {
+  late World world = World();
   @override
   Future<void> onLoad() async {
     TiledComponent map = await TiledComponent.load(
@@ -10,7 +13,14 @@ class SuperMario extends FlameGame {
       Vector2.all(Globals.tileSize),
     );
 
-    add(map);
+    world.add(map);
+
+    var cameraComponent = CameraComponent(world: world)
+    ..viewfinder.visibleGameSize = Vector2(450, 50)
+    ..viewfinder.position = Vector2(0, 0)
+    ..viewfinder.anchor = Anchor.topLeft
+    ..viewfinder.position = Vector2(500, 0)
+
     return super.onLoad();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,27 @@ import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:supermariobros/constants/globals.dart';
 import 'package:supermariobros/constants/sprite_sheets.dart';
+import 'package:supermariobros/games/super_mario_bros_game.dart';
 
-
+final SuperMario _superMarioBrosGame = SuperMario();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Pre-load sprite sheets.
   await SpriteSheets.load();
 
-  await FlameAudio.audioCache.loadAll(
-    [
-      Globals.jumpSmallSFX,
-      Globals.pauseSFX,
-      Globals.bumpSFX,
-      Globals.powerUpAppearsSFX,
-      Globals.breakBlockSFX,
-    ],
-  );
+  await FlameAudio.audioCache.loadAll([
+    Globals.jumpSmallSFX,
+    Globals.pauseSFX,
+    Globals.bumpSFX,
+    Globals.powerUpAppearsSFX,
+    Globals.breakBlockSFX,
+  ]);
 
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Text('Mario'),
-      ),
+      home: GameWidget(game: _superMarioBrosGame),
     ),
   );
 }
