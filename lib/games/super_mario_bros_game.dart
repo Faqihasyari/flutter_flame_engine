5f5f5309aabe0594b2a101a8b436e1cbfa71c59b
Faqihasyari <faqih8158@gmail.com>
2025-08-04 22:06:33 +0700

    update

--- lib/games/super_mario_bros_game.dart
+++ lib/games/super_mario_bros_game.dart
@@ -2,39 +2,30 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:supermariobros/constants/globals.dart';
+import 'package:supermariobros/levels/level_component.dart';
+import 'package:supermariobros/levels/level_option.dart';
 
 class SuperMario extends FlameGame {
-  late World world = World();
+  late CameraComponent cameraComponent;
+  final World world = World();
+  LevelComponent? _currentLevel;
+
   @override
   Future<void> onLoad() async {
-    TiledComponent map = await TiledComponent.load(
-      Globals.lv_1_1,
-      Vector2.all(Globals.tileSize),
-    );
-
-    await world.add(map);
-    await add(world);
-
-    final mapSize = map.size; // Ukuran map (dalam satuan game)
-    final screenSize =
-        size; // Ukuran layar (akan tersedia setelah game layout selesai)
-
-    // Hitung zoom biar seluruh map bisa terlihat
-    final zoomX = screenSize.x / mapSize.x;
-    final zoomY = screenSize.y / mapSize.y;
-    final zoom = zoomX < zoomY ? zoomX : zoomY;
-
     final cameraComponent = CameraComponent(world: world)
       ..viewfinder.zoom =
           4.1 // atur tingkat zoom sesuai keinginanmu
       ..viewfinder.anchor = Anchor.topLeft
       ..viewfinder.position = Vector2.zero();
+    addAll([world, cameraComponent]);
 
-    print('World size: $mapSize');
-    print('Screen size: $screenSize');
-    print('Camera zoom: $zoom');
-    
-    add(cameraComponent);
+    loadLevel(LevelOption.lv_1_1);
     return super.onLoad();
+  }
+
+  void loadLevel(LevelOption option) {
+    _currentLevel?.removeFromParent();
+    _currentLevel = LevelComponent(option);
+    add(_currentLevel!);
   }
 }
