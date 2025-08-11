e8f937671ed67aa884c6fa4f4c6f899dd5a15cb4
Faqihasyari <faqih8158@gmail.com>
2025-08-11 22:31:43 +0700

    push

--- lib/levels/level_component.dart
+++ lib/levels/level_component.dart
@@ -83,13 +83,6 @@ class LevelComponent extends Component with HasGameRef<SuperMario> {
           break;
       }
       // Tambahkan teks posisi ke dunia
-      final positionText = TextComponent(
-        text: 'x: ${obj.x.toInt()}, y: ${obj.y.toInt()}',
-        textRenderer: textPaint,
-        position: Vector2(obj.x, obj.y), // Di atas platform
-        anchor: Anchor.bottomLeft,
-      );
-      gameRef.world.add(positionText);
     }
   }
 
